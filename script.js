1357156135289750a3edbfcfcc2805553b16e543
Rosber Almeida Junior <rosber.almeida@autotrac.corp>
2017-08-21 10:11:57 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("teste");
+var age = Number(prompt("Qual a sua idade?",""));
+
+var i
+var string=""
+for (i=1;i<age;i+1)
+    {
+        string +="Feliz Aniversario\n";
+            
+    }
+alert(string)
